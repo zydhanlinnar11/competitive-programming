0660f70debf45b37e5ceba73c2b81684667e2d11
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2019-12-08 15:53:57 +0700

    Optimize "Group Match" Ga perlu pake int, prototype, dan manual array declaration.

--- TOKI/Kompetitif Dasar TOKI/IV Brute Force/Group Match.cpp
+++ TOKI/Kompetitif Dasar TOKI/IV Brute Force/Group Match.cpp
@@ -1,27 +1,14 @@
 #include <bits/stdc++.h>
 #define nextteam valid(a, b + 1)
+#define us unsigned short
 using namespace std;
-int n, score[] = {0, 0, 0, 0, 0}, scoredata[] = {0, 0, 0, 0, 0};
+us n, score[5] = {0}, scoredata[5] = {0};
 
-bool valid(int, int);
-
-int main() {
-    int t;
-    cin>>t;
-    while(t--) {
-        cin>>n;
-        for(int i=0; i<n; i++) cin>>score[i];
-        if(valid(0,1)) cout<<"YES\n";
-        else cout<<"NO\n";
-    }
-    return 0;
-}
-
-bool valid(int a, int b) {
+bool valid(us a, us b) {
     bool mungkin = false;
     if(a == (n - 1)) {
         mungkin = true;
-        for(int i=0; i<n; i++) {
+        for(us i=0; i<n; i++) {
             if(score[i] != scoredata[i]) return false;
         }
     } else if(b == n) return (mungkin || valid(a + 1, a + 2));
@@ -41,4 +28,16 @@ bool valid(int a, int b) {
         scoredata[b] -= 3;
     }
     return mungkin;
+}
+
+int main() {
+    us t;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        for(us i=0; i<n; i++) cin>>score[i];
+        if(valid(0,1)) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
 }
