107804d3a7237456cddaf1691f0585e71d6dffb0
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2019-12-19 20:24:37 +0700

    Update BAB IV: Brute Force (Jawbreaker III) Optimize code.

--- TOKI/Kompetitif Dasar TOKI/IV Brute Force/Jawbreaker III.cpp
+++ TOKI/Kompetitif Dasar TOKI/IV Brute Force/Jawbreaker III.cpp
@@ -1,72 +1,77 @@
 #include <bits/stdc++.h>
-
+#define sizebool(x) (sizeof(x)/sizeof(bool))
+#define sizeshort(x) (sizeof(x)/sizeof(short))
+#define set0(x, z) memset(x, 0, z)
 using namespace std;
 
-short movex[] = {0, 0, 1, -1}, movey[] = {1, -1, 0, 0}, bola[25][25], m, n, xmax, ymax, runtuh[25] = {0};
-int nilai, maks;
-bool visited[25][25] = {0}, mark;
+short movex[] = {0, 0, 1, -1}, movey[] = {1, -1, 0, 0}, m, n;
+int nilai, maks = -1;
 
-void count(short x, short y, short ball) {
-    if((bola[x][y] != ball) || ((mark) ? false : visited[x][y]) || (y < 0) || (x < 0) || (y >= 25) || (x >= 25)) return;
+void count(short x, short y, short ball, short bola[25][25], bool visited[25][25], bool runtuhkan) {
+    if((bola[x][y] != ball) || ((runtuhkan) ? false : visited[x][y]) || (y < 0) || (x < 0) || (y >= 25) || (x >= 25)) return;
     else {
-        if(mark) bola[x][y] = -1;
-        else {
-            visited[x][y] = true;
-            nilai++;
+        nilai++;
+        if(runtuhkan) bola[x][y] = -1;
+        else visited[x][y] = true;
+        for(int i=0; i < sizeshort(movex); i++) {
+            count(x + movex[i], y + movey[i], ball, bola, visited, runtuhkan);
         }
-        for(int i=0; i < (sizeof(movex) / sizeof(short)); i++) {
-            count(x + movex[i], y + movey[i], ball);
+    }
+}
+
+void runtuh(short bola[25][25]) {
+    for(int i=0; i<n; i++) {
+        bool ada = true;
+        while(ada) {
+            ada = false;
+            for(int j=0; j<(m - 1); j++) {
+                if(bola[j][i] != -1 && bola[j+1][i] == -1) {
+                    ada = true;
+                    bola[j][i] ^= bola[j+1][i];
+                    bola[j+1][i] ^= bola[j][i];
+                    bola[j][i] ^= bola[j+1][i];
+                }
+            }
+        }
+    }
+}
+
+void printmatrix(short bola[25][25]) {
+    for(int a=0; a<m; a++) {
+        for(int b=0; b<n; b++) {
+            if(bola[a][b] != -1) cout<<bola[a][b];
+            else cout<<'.';
+            if(b < n - 1) cout<<' ';
+            else cout<<'\n';
         }
     }
 }
 
 int main() {
-    maks = -1;
+    short bola[25][25];
+    bool visited[25][25] = {0};
+    int x, y;
     cin>>m>>n;
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++)
             cin>>bola[i][j];
     }
-    mark = false;
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
             if(!visited[i][j]) {
                 nilai = 0;
-                count(i, j, bola[i][j]);
-                if (nilai > maks) {
+                count(i, j, bola[i][j], bola, visited, false);
+                if(nilai > maks) {
                     maks = nilai;
-                    xmax = i;
-                    ymax = j;
+                    x = i;
+                    y = j;
                 }
             }
         }
     }
-    mark = true;
-    count(xmax, ymax, bola[xmax][ymax]);
-    for(int i = 0; i < n; i++) {
-        int marked = 0, indexy = 0;
-        for(int j = 0; j < m; j++) {
-            if(bola[j][i] == -1) marked++;
-            else {
-                runtuh[indexy] = bola[j][i];
-                indexy++;
-            }
-        }
-        for(int j = 0; j < marked; j++) {
-            bola[j][i] = -1;
-        }
-        int k = 0;
-        for(int j = marked; j <= marked+indexy; j++, k++) {
-            bola[j][i] =  runtuh[k];
-        }
-    }
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            if(bola[i][j] == -1) cout<<'.';
-            else cout<<bola[i][j];
-            if(j == n-1) cout<<endl;
-            else cout<<' ';
-        }
-    }
+    set0(visited, sizebool(visited));
+    count(x, y, bola[x][y], bola, visited, true);
+    runtuh(bola);
+    printmatrix(bola);
     return 0;
 }
