1ed262793b9bc3c7088fb438afc7b63aa93142c7
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2019-12-25 00:52:42 +0700

    Update Codeforces Round #610 Div. 2 Optimize code

--- Codeforces/CF Round #610 Div 2/Temporarily unavailable.cpp
+++ Codeforces/CF Round #610 Div 2/Temporarily unavailable.cpp
@@ -8,25 +8,24 @@ int main() {
     cin>>t;
     while(t--) {
         cin>>a>>b>>c>>r;
-        long long before = 0, after = 0, left = c - r, right = c + r;
+        long long left = c - r, right = c + r;
         if(a > b) {
             a ^= b;
             b ^= a;
             a ^= b;
         }
-        if(left >= a && left <= b && right <=b && right >=a) {
+        if(left >= a && left <= b && right <=b && right >=a)
             cout<<(b - right) + (left - a)<<'\n';
-        } else if(left>=a && left <= b && !(right <=b && right >=a)) {
+        else if(left>=a && left <= b && !(right <=b && right >=a))
             cout<<(left - a)<<'\n';
-        } else if(!(left>=a && left <= b) && !(right <=b && right >=a)) {
+        else if(!(left>=a && left <= b) && !(right <=b && right >=a)) {
             if((left < a && right < a) || (left > b && right > b)) {
                 cout<<(b - a)<<'\n';
             } else {
                 cout<<"0\n";
             }
-        } else if(!(left>=a && left <= b) && (right <=b && right >=a)) {
+        } else if(!(left>=a && left <= b) && (right <=b && right >=a))
             cout<<(b - right)<<'\n';
-        }
     }
     return 0;
 }
