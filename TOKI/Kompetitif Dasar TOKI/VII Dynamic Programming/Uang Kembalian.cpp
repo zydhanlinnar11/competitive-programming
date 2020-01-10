198505de1f49255f2db835d53ec77c04427863ec
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2020-01-11 00:17:47 +0700

    Update BAB VII (DP) : Uang Kembalian Udah AC.

--- TOKI/Kompetitif Dasar TOKI/VII Dynamic Programming/Uang Kembalian.cpp
+++ TOKI/Kompetitif Dasar TOKI/VII Dynamic Programming/Uang Kembalian.cpp
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool filled[500] = {0}, exist = false;
-int dp[500] = {0}, n, k, coin[500];
+typedef long long ll;
+bool filled[50000] = {0};
+ll dp[50000] = {0};
+unsigned short n, k, coin[500];
 
-int tukar(int x) {
-    if(x == 0) {
-        exist = true;
-        return 0;
+ll tukar(int x) {
+    if(x == 0) return 0;
+    if(filled[x - 1]) return dp[x - 1];
+    if(x < coin[0]) {
+        filled[x - 1] = true;
+        dp[x - 1] = INT_MAX;
     }
-    if(filled[x]) return dp[x];
-    int min = INT_MAX;
-    bool subexist = false;
-    for(int i=0; i<n; i++) {
-        if(coin[i] <= x) {
-            subexist = true;
-            int tmp = 1 + tukar(x - coin[i]);
-            min = (tmp < min && tmp != -1) ? tmp : min;
-        } else if(i == (n - 1) && !subexist) {
-            filled[x] = true;
-            dp[x] = -1;
-            return -1;
+    ll min = INT_MAX;
+    for(unsigned short i=0; i<n; i++) {
+        if(coin[i] <= x){
+            ll val = tukar(x - coin[i]) + 1;
+            if(val < min) min = val;
         }
     }
-    filled[x] = true;
-    dp[x] = min;
+    filled[x - 1] = true;
+    dp[x - 1] = min;
     return min;
 }
 
@@ -31,10 +28,10 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n;
-    for(int i=0; i<n; i++)
+    for(short i=0; i<n; i++)
         cin>>coin[i];
     cin>>k;
-    int val = tukar(k);
-    cout<<((exist) ? (val) : (-1))<<'\n';
+    ll val = tukar(k);
+    cout<<((val >= INT_MAX) ? -1 : val)<<'\n';
     return 0;
 }
