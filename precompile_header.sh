ddc8665bde7fd42b5d052fc963d7a4032db9e8b7
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2021-11-24 16:50:59 +0700

    Add precompiled header stuff

---
+++ precompile_header.sh
@@ -0,0 +1,3 @@
+sudo g++ -std=c++17 -Wshadow -Wall -Wextra -Wno-unused-result -O2 -DZYD_WSL -g /usr/include/c++/10/ext/pb_ds/assoc_container.hpp
+sudo g++ -std=c++17 -Wshadow -Wall -Wextra -Wno-unused-result -O2 -DZYD_WSL -g /usr/include/c++/10/ext/pb_ds/tree_policy.hpp
+sudo g++ -std=c++17 -Wshadow -Wall -Wextra -Wno-unused-result -O2 -DZYD_WSL -g /usr/include/x86_64-linux-gnu/c++/10/bits/stdc++.h
