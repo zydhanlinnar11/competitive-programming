b380e7d62bf36f01fffbbc01e130a13891aecf5e
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-07-11 11:23:18 +0700

    add cf 805 and 1486b

---
+++ CF/1486B.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define mp(x, y) make_pair(x, y)
+using namespace std;
+using namespace chrono;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vi2d;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef vector<vl> vl2d;
+typedef vector<string> vs;
+typedef vector<vs> vs2d;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+time_point<system_clock> chrono_time_start, chrono_time_end;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define MOD 1000000007
+
+inline ll modmul(ll a, ll b, ll mod = MOD) {
+    return ((a % mod) * (b % mod)) % mod;
+}
+
+inline void prog() {
+    int n; cin>>n;
+    vii coords(n);
+    for(int i=0; i<n; i++) {
+        auto &ci = coords[i];
+        cin>>ci.first>>ci.second;
+    }
+    map<ll, set<int>> p1, p2;
+    for(int i=0; i<n; i++) {
+        ll res = 0;
+        for(int j=0; j<n; j++) {
+            if(i == j) continue;
+            res += abs(coords[i].first - coords[j].first);
+        }
+        p1[res].insert(coords[i].first);
+    }
+    for(int i=0; i<n; i++) {
+        ll res = 0;
+        for(int j=0; j<n; j++) {
+            if(i == j) continue;
+            res += abs(coords[i].second - coords[j].second);
+        }
+        p2[res].insert(coords[i].second);
+    }
+    ll ans = *p1.begin()->second.rbegin() - *p1.begin()->second.begin() + 1;
+    ans *= *p2.begin()->second.rbegin() - *p2.begin()->second.begin() + 1;
+    cout<<ans<<"\n";
+}
+
+int main() {
+    chrono_time_start = system_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef ZYD_WSL
+        freopen("/home/zydhanlinnar11/cp/CF/in", "r", stdin);
+        freopen("/home/zydhanlinnar11/cp/CF/out", "w", stdout);
+    #endif
+    int t = 1;
+    cin>>t;
+    while(t--) prog();
+    chrono_time_end = system_clock::now();
+    duration<double> elapsed = chrono_time_end - chrono_time_start;
+    // cout<<"Time elapsed: "<<setprecision(3)<<fixed<<elapsed.count() * 1000<<" ms.\n";
+    return 0;
+}
